f3c90bf834bf96f1f233acf16b5792338dba5684
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-03-10 17:23:02 +0000

    init

---
+++ gmail-clone/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <h1>Lets build Gmail</h1>
+      
+    </div>
+  );
+}
+
+export default App;
