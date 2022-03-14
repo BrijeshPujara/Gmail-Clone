57a0084a677bb354182a31f90358355d3ca0f72d
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-03-14 18:52:38 +0000

    Sidebar designed

--- gmail-clone/src/components/Header.js
+++ gmail-clone/src/components/Header.js
@@ -1,11 +1,42 @@
 import React from "react";
 import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 function Header() {
-    return <div className="header">
-    <h1>This is the header</h1> 
-    
-    </div>;
+  return (
+    <div className="header">
+      <div className="header-left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
+          alt=""
+        />
+      </div>
+      <div className="header-middle">
+        <SearchIcon />
+        <input type="text" placeholder="Search mail" />
+        <IconButton>
+          <ArrowDropDownIcon />
+        </IconButton>
+      </div>
+      <div className="header-right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+              </IconButton>
+              <Avatar />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
---
+++ gmail-clone/src/components/Sidebar.js
@@ -0,0 +1,40 @@
+import { Button } from "@mui/material";
+import React from "react";
+import "./Sidebar.css";
+import AddIcon from "@mui/icons-material/Add";
+import InboxIcon from "@mui/icons-material/Inbox";
+import StarIcon from "@mui/icons-material/Star";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import NoteIcon from "@mui/icons-material/Note";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import SidebarOption from "./SidebarOption";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <Button
+        startIcon={<AddIcon fontSize="large" />}
+        className="sidebar-compose"
+      >
+        Compose
+      </Button>
+
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={54} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={54} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="Categories" number={54} />
+    </div>
+  );
+}
+
+export default Sidebar;
